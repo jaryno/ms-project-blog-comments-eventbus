005a5f96520f1ceaa4cd9a05bc2a560ac6bdbc3e
Jaroslav Zhanel <zhanel@tech4bet.com>
2021-10-22 13:30:36 +0200

    new

--- index.js
+++ index.js
@@ -3,20 +3,38 @@ const bodyParser = require('body-parser');
 const axios = require('axios');
 
 // const { randomBytes } = require("crypto");
-// const cors = require('cors');
+const cors = require('cors');
 
 const app = express();
 app.use(express.json());
 // app.use(cors());
 
+const events = [];
+
 app.post('/events', (req, res) => {
     const event = req.body;
+    // console.log('Reicived event', req.body.type);
 
-    axios.post('http://localhost:4000/events', event);
-    axios.post('http://localhost:4001/events', event);
-    axios.post('http://localhost:4002/events', event);
-    
+    events.push(event);
+
+    axios.post('http://localhost:4000/events', event).catch((err) => {
+        console.log(err.message);
+    });
+    axios.post('http://localhost:4001/events', event).catch((err) => {
+        console.log(err.message);
+    });
+    axios.post('http://localhost:4002/events', event).catch((err) => {
+        console.log(err.message);
+    });
+    axios.post('http://localhost:4003/events', event).catch((err) => {
+        console.log(err.message);
+    });
+
     res.send({ status: 'ok' });
+});
+
+app.get('/events', (req, res) => {
+    res.send(events);
 });
 
 app.listen(4005, () => {
